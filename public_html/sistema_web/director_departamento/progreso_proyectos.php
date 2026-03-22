9737a50ba07c9fc8f0dc1db6c40bcf5abf9aa7b3
Luigi Villanueva <luigi13.livp@gmail.com>
2026-03-22 12:18:27 -0500

    Migrar sidebar dinamico para director_departamento

--- public_html/sistema_web/director_departamento/progreso_proyectos.php
+++ public_html/sistema_web/director_departamento/progreso_proyectos.php
@@ -108,45 +108,7 @@ include('../componentes/db.php');
             </ul>
         </nav>
         <!-- Sidebar -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <a href="inicio.php" class="brand-link">
-                <img src="../dust/img/dirsu_logo_128_128.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">Sistema DIRSU</span>
-            </a>
-            <div class="sidebar">
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="../dust/img/avatar.png" class="img-circle elevation-2" alt="User Image">
-                    </div>
-                    <div class="info">
-                        <a href="inicio.php" class="d-block"><?php echo htmlspecialchars($nombres) . " " . htmlspecialchars($apellidos); ?></a>
-                    </div>
-                </div>
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                        <li class="nav-item">
-                            <a href="inicio.php" class="nav-link">
-                                <i class="nav-icon fas fa-home"></i>
-                                <p>INICIO</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="progreso_proyectos.php" class="nav-link active">
-                                <i class="fa fa-chart-line nav-icon"></i>
-                                <p>Progreso de Proyectos</p>
-                            </a>
-                        </li>
-                        <li class="nav-header">Evaluación de Proyectos</li>
-                        <li class="nav-item">
-                        <a href="visto.php" class="nav-link">
-                           <i class="fa fa-eye nav-icon"></i><i class="fa fa-check nav-icon"></i>
-                           <p>Visto Bueno</p>
-                        </a>
-                     </li>
-                    </ul>
-                </nav>
-            </div>
-        </aside>
+        <?php include_once __DIR__ . '/../includes/sidebar.php'; ?>
         <div class="content-wrapper">
             <section class="content" style="height: 400px;">
                 <div class="card">
--- public_html/sistema_web/director_departamento/visto.php
+++ public_html/sistema_web/director_departamento/visto.php
@@ -48,64 +48,7 @@ include('../componentes/db.php');
             </ul>
         </nav>
         <!-- Sidebar -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <a href="inicio.php" class="brand-link">
-                <img src="../dust/img/dirsu_logo_128_128.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">Sistema DIRSU</span>
-            </a>
-            <div class="sidebar">
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="../dust/img/avatar.png" class="img-circle elevation-2" alt="User Image">
-                    </div>
-                    <div class="info">
-                        <a href="inicio.php" class="d-block"><?php echo htmlspecialchars($nombres) . " " . htmlspecialchars($apellidos); ?></a>
-                    </div>
-                </div>
-                <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-              <!-- Add icons to the links using the .nav-icon class
-                with font-awesome or any other icon font library -->
-              <li class="nav-item">
-                <a href="inicio.php" class="nav-link">
-                  <i class="nav-icon fas fa-home"></i>
-                  <p>
-                    INICIO
-                    <!-- <span class="right badge badge-danger">2</span> -->
-                  </p>
-                </a>
-              </li>
-              <!-- <li class="nav-item">
-                <a href="progreso_proyectos.php" class="nav-link">
-                   <i class="fa fa-chart-line nav-icon"></i>
-                   <p>Progreso de Proyectos</p>
-                </a>
-                </li> -->
-              <li class="nav-header">Evaluación de Proyectos</li>
-              <li class="nav-item">
-                <a href="evaluacion.php" class="nav-link">
-                  <i class="fa fa-calendar nav-icon"></i>
-                  <p>Informe Semestral 2025-I</p>
-                </a>
-              </li>
-              <li class="nav-item menu menu-open">
-                <a href="#" class="nav-link active">
-                  <span class="badge nav-icon"><i class="fas fa-user-cog"></i></span>
-                  <p>Evaluaciones anteriores</p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="visto.php" class="nav-link active">
-                      <i class="fa fa-clipboard-list nav-icon"></i>
-                      <p>Visto Bueno (2024)</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </nav> 
-            </div>
-        </aside>
+        <?php include_once __DIR__ . '/../includes/sidebar.php'; ?>
         <div class="content-wrapper">
         <section class="content p-3">
         <?php
